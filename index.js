2918dc0561c1a4d85d36f69711d294f34f24d24b
Pooja Kumari <pooja_fw10_055@masai.school>
2024-02-24 00:10:00 +0530

    fixed cors error

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ async function translater() {
 
     let que = document.getElementById('inp').value;
     let language_select = document.getElementById("lang").value;
-    console.log(language_select);
     const res = await fetch("https://libretranslate.de/translate", {
-
         method: "POST",
         body: JSON.stringify({
             q: que,
             source: "en",
             target: language_select,
         }),
-        headers: { "Content-Type": "application/json" }
+        headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Credentials": true }
     });
 
-
     let r = await res.json();
-    console.log(r);
     var output = document.getElementById('output');
     output.innerHTML = r.translatedText;
 }
